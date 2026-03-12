561d53e531c52e55955963c98c85b9ed61a7014b
aleandra-xh <xhaferajaleandra@gmail.com>
2026-03-12 09:47:13 +0100

    Add validation rules for property search query

--- Booking.Application/Features/Properties/SearchProperties/SearchPropertiesQueryValidator.cs
+++ Booking.Application/Features/Properties/SearchProperties/SearchPropertiesQueryValidator.cs
@@ -1,2 +1,31 @@
-﻿
+﻿using Booking.Domain.Properties;
+using FluentValidation;
+
 namespace Booking.Application.Features.Properties.SearchProperties;
+
+public sealed class SearchPropertiesQueryValidator : AbstractValidator<SearchPropertiesQuery>
+{
+    public SearchPropertiesQueryValidator()
+    {
+        RuleFor(x => x.Request.Page)
+            .GreaterThan(0)
+            .WithMessage("Page must be greater than 0.");
+
+        RuleFor(x => x.Request.PageSize)
+            .InclusiveBetween(1, 50)
+            .WithMessage("Page size must be between 1 and 50.");
+
+        RuleFor(x => x.Request.PropertyType)
+            .Must(value => value is null || Enum.IsDefined(typeof(PropertyType), value.Value))
+            .WithMessage("Invalid property type.");
+
+        RuleFor(x => x.Request.MaxGuests)
+            .GreaterThan(0)
+            .When(x => x.Request.MaxGuests.HasValue)
+            .WithMessage("Max guests must be greater than 0.");
+
+        RuleFor(x => x.Request.City)
+            .MaximumLength(100)
+            .When(x => !string.IsNullOrWhiteSpace(x.Request.City));
+    }
+}
